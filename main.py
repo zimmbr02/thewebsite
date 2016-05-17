5668429fc811fc292d2333c4609e84834eb657d0
Ryne Hanson <hansry02@luther.edu>
2016-05-17 03:43:25 -0500

    5/17/2016 Added many features

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy import Column, Integer, String, Date, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import update
 
 import json
 import flask.ext.login as flask_login
@@ -28,7 +29,7 @@ class Users(Base):
     username = Column(String(20))
     password = Column(String(20))
     question = Column(Integer)
-    answer = Column(String(100))
+    answer = Column(String(100))	
     
 Base.metadata.create_all(engine)
 
@@ -56,13 +57,13 @@ Bootstrap(app)
 		#return f(*args, **kwargs)
 	#return decorated_function
 	
-users = []
+theUsers = []
 for item in session.query(Users).all():
-	users.append(item.username)
+	theUsers.append(item.username)
 	
 @login_manager.user_loader
 def load_user(username):
-	if username not in users:
+	if username not in theUsers:
 		return
 	user = User()
 	user.id = username
@@ -99,6 +100,22 @@ def home(username):
 	else:
 		return redirect(url_for('logOut'))
 		
+@app.route('/toDo.html/<username>')
+def toDo(username):
+	if flask_login.current_user.get_id() == username:
+		return render_template('toDo.html', user=username)
+		
+	else:
+		return redirect(url_for('logOut'))
+		
+@app.route('/accountSettings.html/<username>')
+def accountSettings(username):
+	if flask_login.current_user.get_id() == username:
+		return render_template('accountSettings.html', user=username)
+		
+	else:
+		return redirect(url_for('logOut'))
+		
 @app.route('/midtermMashup.html')
 def mashup():
 	return render_template('midtermMashup.html')
@@ -117,6 +134,56 @@ def logIn():
 def logOut():
 	flask_login.logout_user()
 	return redirect(url_for('index'))
+	
+@app.route('/confirmPassword', methods=['POST'])
+def confirmPassword():
+	theDict = json.loads(request.data.decode("utf8"))
+	sentPassword = theDict['sentPassword']
+	
+	allUsers = []
+	match = False
+	for item in session.query(Users).all():
+		allUsers.append(item.username)
+	
+	if flask_login.current_user.get_id() in allUsers:
+		if session.query(Users).filter_by(username = flask_login.current_user.get_id()).all()[0].password == sentPassword:
+			match = True
+		
+	return jsonify(tasklist = [match])
+	
+@app.route('/updatePassword', methods=['POST'])
+def updatePassword():
+	theDict = json.loads(request.data.decode("utf8"))
+	sentPassword = theDict['sentPassword']
+	currentUsername = flask_login.current_user.get_id()
+	
+	currentUserData = session.query(Users).filter_by(username = currentUsername).first()
+	currentUserData.password = sentPassword
+	session.commit()
+	
+	return jsonify (tasklist = [True])
+	
+@app.route('/deleteAccount', methods=['POST'])
+def deleteAccount():
+	currentUsername = flask_login.current_user.get_id()
+	
+	currentUserData = session.query(Users).filter_by(username = currentUsername).first()
+	session.delete(currentUserData)
+	session.commit()
+	
+	flask_login.logout_user()
+	
+	return jsonify (tasklist = [True])
+	
+@app.route('/personalData', methods=['GET'])
+def sendAllPersonalData():
+	results = session.query(Users).all()
+	reslist = []
+	
+	for item in results:
+		reslist.append(dict(id=item.id,sentUsername=item.username,sentPassword=item.password))
+		
+	return jsonify(tasklist = reslist)
 	
 #renders sign-up.html
 @app.route('/sign-up.html')
--- static/myScript.js
+++ static/myScript.js
@@ -1,3 +1,7 @@
+var showData = false;
+var showChangePassword = false;
+var showDeleteAccount = false;
+
 answerPresent = function() {
 	answer = document.querySelector('#answer').value;
 
@@ -60,6 +64,81 @@ createAccount = function() {
 	}
 }
 
+changePassword = function() {
+	oldPassword = document.querySelector('#oldPassword').value;
+	
+	var req = new XMLHttpRequest();
+	req.open("POST", "/confirmPassword", true);
+	req.setRequestHeader("Content-type", "application/json");
+	var data = {sentPassword:oldPassword};
+	data.done = true;
+	req.send(JSON.stringify(data));
+	req.onreadystatechange = function() {
+		if (req.readyState == 4 && req.status == 200) {
+			confirmation = JSON.parse(req.responseText);
+			if (confirmation.tasklist[0] != true) {
+				p = document.querySelector('.incorrect');
+				p.innerHTML = 'Incorrect password';
+				p.style.color = "red";
+			}
+					
+			else {
+				settings = document.querySelector('#accountSettings');
+				settings.innerHTML = "";
+				
+				form = document.createElement('form');
+				form.role = "form";
+				newDiv = document.createElement('div');
+				newDiv.classList.add("form-group");
+				newLabel = document.createElement('label');
+				newLabel.htmlFor = "password";
+				newLabel.id = "checkPassword";
+				newLabel.innerHTML = "New Password:";
+				newInput = document.createElement('input');
+				newInput.type = "password";
+				newInput.classList.add("form-control");
+				newInput.id = "password";
+				newInput.oninput = passwordMatch;
+				
+				newDiv.appendChild(newLabel);
+				newDiv.appendChild(newInput);
+				form.appendChild(newDiv);
+				
+				confirmDiv = document.createElement('div');
+				confirmDiv.classList.add("form-group");
+				confirmLabel = document.createElement('label');
+				confirmLabel.htmlFor = "password";
+				confirmLabel.id = "checkConfirmPassword";
+				confirmLabel.innerHTML = "Confirm Password:";
+				confirmInput = document.createElement('input');
+				confirmInput.type = "password";
+				confirmInput.classList.add("form-control");
+				confirmInput.id = "confirmPassword";
+				confirmInput.oninput = passwordMatch;
+				
+				confirmDiv.appendChild(confirmLabel);
+				confirmDiv.appendChild(confirmInput);
+				form.appendChild(confirmDiv);
+				
+				
+				button = document.createElement('button');
+				button.type = "button";
+				button.classList.add("btn");
+				button.classList.add("btn-primary");
+				button.onclick = updatePassword;
+				button.innerHTML = "Change Password";
+				p = document.createElement('p');
+				p.classList.add("incorrect");
+
+				settings.appendChild(form);
+				settings.appendChild(button);
+				settings.appendChild(p);
+				
+			}
+		}
+	}
+}
+
 checkQuestion = function() {
 	select = document.querySelector('#sel1');
 	checkSecurity = document.querySelector('#checkSecurity');
@@ -108,13 +187,58 @@ checkUsernameAvailability = function() {
 	}
 }
 
+deleteAccount = function() {
+	oldPassword = document.querySelector('#oldPassword').value;
+	
+	var req = new XMLHttpRequest();
+	req.open("POST", "/confirmPassword", true);
+	req.setRequestHeader("Content-type", "application/json");
+	var data = {sentPassword:oldPassword};
+	data.done = true;
+	req.send(JSON.stringify(data));
+	req.onreadystatechange = function() {
+		if (req.readyState == 4 && req.status == 200) {
+			confirmation = JSON.parse(req.responseText);
+			if (confirmation.tasklist[0] != true) {
+				p = document.querySelector('.incorrect');
+				p.innerHTML = 'Incorrect password';
+				p.style.color = "red";
+			}
+					
+			else {
+				if (confirm("Are you sure you wish to delete your account? This action CANNOT be undone!")) {
+					var req2 = new XMLHttpRequest();
+					req2.open("POST", "/deleteAccount", true);
+					req2.setRequestHeader("Content-type", "application/json");
+					var data2 = {};
+					data2.done = true;
+					req2.send(JSON.stringify(data2));
+					req2.onreadystatechange = function() {
+						if (req2.readyState == 4 && req2.status == 200) {
+							confirmation = JSON.parse(req2.responseText);
+							if (confirmation.tasklist[0] == true) {
+								alert("Account Deleted.");
+								window.location = "/";
+							}
+								
+							else {
+								alert("Oops! Something went wrong!");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 /* The forgotPassword.html page calls this when a user clicks the fetchQuestion button.
 	This function makes a request to the server to get the username's matching security question.
 	If it finds a match, it will give the user a chance to answer the question. 
 	If the user answers correctly, the user will be able to enter a new password.
 	If any of the info is incorrect, an error message will be displayed. */
 fetchQuestion = function() {
-	
+	alert("Oops! This isn't working yet :/");
 }
 
 /* The index.html page calls this when the user clicks the logIn button.
@@ -152,18 +276,6 @@ logIn = function() {
 				window.location = "home.html/" + username;
 			}
 		}
-	}
-}
-
-logInRedirector = function() {
-	logButton = document.querySelector('#logButton');
-	
-	if (logButton.text == " Log-in") {
-		window.location = "log-in.html";
-	}
-	
-	else {
-		window.location = "/";
 	}
 }
 
@@ -205,6 +317,35 @@ restoreLocalUserInfo = function() {
 	password.value = rememberedPassword;
 }
 
+showAllUsersPersonalInfo = function() {
+	tbody = document.querySelector('#personalDataBody');
+	
+	var req = new XMLHttpRequest();
+	req.open("GET", "/personalData", true);
+	req.onreadystatechange = function() {
+		if (req.readyState == 4 && req.status == 200) {
+			users = JSON.parse(req.responseText);
+			if (! showData) {
+				for (user in users.tasklist) {
+					tr = document.createElement('tr');
+					columnUsername = document.createElement('td');
+					columnPassword = document.createElement('td');
+					columnUsername.appendChild(document.createTextNode(users.tasklist[user].sentUsername));
+					columnPassword.appendChild(document.createTextNode(users.tasklist[user].sentPassword));
+					tr.appendChild(columnUsername);
+					tr.appendChild(columnPassword);
+					tbody.appendChild(tr);
+				}
+				table = document.querySelector('#personalData');
+				table.style.display = "table";
+				showData = true;
+			}
+			else {}
+		}
+	}
+	req.send()
+}
+
 //The sign-up.html page calls this when the user checks showPassword.
 showPassword = function() {
 	if (document.querySelector('#showPassword').checked) {
@@ -224,5 +365,122 @@ showAnswer = function() {
 	}
 	else {
 		document.querySelector('#answer').type = "password";
+	}
+}
+
+showChangePasswordInput = function() {
+
+	settings = document.querySelector('#accountSettings');
+	
+	//settings.innerHTML = "";
+	
+	if (! showChangePassword) {
+	
+		form = document.createElement('form');
+		form.role = "form";
+		div = document.createElement('div');
+		div.classList.add("form-group");
+		label = document.createElement('label');
+		label.htmlFor = "oldPassword";
+		label.innerHTML = "Old Password:";
+		input = document.createElement('input');
+		input.type = "password";
+		input.classList.add("form-control");
+		input.id = "oldPassword";
+		button = document.createElement('button');
+		button.type = "button";
+		button.classList.add("btn");
+		button.classList.add("btn-primary");
+		button.onclick = changePassword;
+		button.innerHTML = "Change Password";
+		p = document.createElement('p');
+		p.classList.add("incorrect");
+		
+		div.appendChild(label);
+		div.appendChild(input);
+		form.appendChild(div);
+		settings.appendChild(form);
+		settings.appendChild(button);
+		settings.appendChild(p);
+		showChangePassword = true;
+	}
+	
+	else {}
+}
+
+showDeleteAccountInput = function() {
+
+	settings = document.querySelector('#accountSettings');
+	
+	//settings.innerHTML = "";
+	
+	if (! showDeleteAccount) {
+	
+		form = document.createElement('form');
+		form.role = "form";
+		div = document.createElement('div');
+		div.classList.add("form-group");
+		label = document.createElement('label');
+		label.htmlFor = "oldPassword";
+		label.innerHTML = "Password:";
+		input = document.createElement('input');
+		input.type = "password";
+		input.classList.add("form-control");
+		input.id = "oldPassword";
+		button = document.createElement('button');
+		button.type = "button";
+		button.classList.add("btn");
+		button.classList.add("btn-danger");
+		button.onclick = deleteAccount;
+		button.innerHTML = "Delete Account";
+		p = document.createElement('p');
+		p.classList.add("incorrect");
+		
+		div.appendChild(label);
+		div.appendChild(input);
+		form.appendChild(div);
+		settings.appendChild(form);
+		settings.appendChild(button);
+		settings.appendChild(p);
+		showDeleteAccount = true;
+	}
+	
+	else {}
+}
+
+updatePassword = function() {
+	checkPassword = document.querySelector('#checkPassword');
+	checkConfirmPassword = document.querySelector('#checkConfirmPassword');
+	
+	if (checkPassword.style.color == checkConfirmPassword.style.color && checkPassword.style.color == "green") {
+	
+		password = document.querySelector('#password').value;
+	
+		var req = new XMLHttpRequest();
+		req.open("POST", "/updatePassword", true);
+		req.setRequestHeader("Content-type", "application/json");
+		var data = {sentPassword:password};
+		data.done = true;
+		req.send(JSON.stringify(data));
+		req.onreadystatechange = function() {
+			if (req.readyState == 4 && req.status == 200) {
+				confirmation = JSON.parse(req.responseText);
+				if (confirmation.tasklist[0] == true) {
+					p = document.querySelector('.incorrect');
+					p.innerHTML = 'Password Updated Successfully!';
+					p.style.color = "green";
+				}
+					
+				else {
+					alert("Oops! Something went wrong!");
+				}
+			}
+		}
+	}
+	
+	else {
+		p = document.querySelector('.incorrect');
+		p.innerHTML = 'Passwords do not match!';
+		p.style.color = "red";
 	}
 }
